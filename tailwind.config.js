60a08e7127ebcecd69b561571381e322a40d19aa
ninazmul <nazmulsaw@gmail.com>
2024-01-23 15:30:26 +0600

    home page stracture done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'red': '#ea0000',
-        'blue': '#2a68f8',
-        'dark': '#282828',
-        'gray': '#666',
-        'lightGray': '#727272',
-        'darkWhite': '#ddd',
+        red: "#ea0000",
+        blue: "#2a68f8",
+        dark: "#282828",
+        gray: "#666",
+        lightGray: "#727272",
+        darkWhite: "#ddd",
       },
       boxShadow: {
-        'custom': '0px 16px 48px rgba(39, 39, 39, 0.1)',
-      }
+        custom: "0px 16px 48px rgba(39, 39, 39, 0.1)",
+      },
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: '1rem',
+        DEFAULT: "1rem",
       },
     },
     screens: {
-      sm: '540px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1320px',
+      sm: "540px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1320px",
     },
     fontFamily: {
-      'PlayFair': ['Playfair Display', "serif"]
-    }
+      PlayFair: ["Playfair Display", "serif"],
+    },
   },
   plugins: [require("daisyui")],
-=======
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> 6b830c3c07ee179a4919f30b7af24657434427b1
-}
-
+};
